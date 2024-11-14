e8159cceae7d794cda9e5eaf5f5e013dcb37f778
manikantapennada <pennadamanikanta1@gmail.com>
2024-11-14 10:31:45 +0530

    added api postman

--- api/views.py
+++ api/views.py
@@ -3,7 +3,49 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from api.models import product
+from rest_framework import status
+from .serializers import ProductSerializer
 
 @api_view()
 def getData(request):
     return Response({"message":"Hello World"})
+
+class ProductList(APIView):
+    def get(self,request):
+        products = product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+    def post(self,request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=200)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+
+class ProductDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return product.objects.get(pk=pk)
+        except product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
